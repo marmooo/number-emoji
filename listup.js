b824f51f6cf809eb64538c588b20f101e44067cc
marmooo <marmooo@users.noreply.github.com>
2024-02-24 01:02:45 +0900

    remove SVG containng text nodes

--- listup.js
+++ listup.js
@@ -1,5 +1,16 @@
 import { expandGlobSync } from "https://deno.land/std/fs/expand_glob.ts";
 
+function blobmoji(baseDir, dir) {
+  const paths = [];
+  for (const file of expandGlobSync(`${baseDir}/${dir}/*.svg`)) {
+    const svg = Deno.readTextFileSync(file.path);
+    if (svg.includes("<text")) continue;
+    const path = file.path.slice(baseDir.length + dir.length + 2);
+    paths.push(path);
+  }
+  Deno.writeTextFileSync(`src/data/${dir}.txt`, paths.join("\n"));
+}
+
 const dirs = [
   "noto-emoji",
   "twemoji",
@@ -13,11 +24,20 @@ const dirs = [
 ];
 const baseDir = Deno.realPathSync("svg");
 for (const dir of dirs) {
-  const paths = [];
-  const glob = expandGlobSync(`${baseDir}/${dir}/**/*.svg`, { globstar: true });
-  for (const file of glob) {
-    const path = file.path.slice(baseDir.length + dir.length + 2);
-    paths.push(path);
+  switch (dir) {
+    case "blobmoji":
+      blobmoji(baseDir, dir);
+      break;
+    default: {
+      const paths = [];
+      const glob = expandGlobSync(`${baseDir}/${dir}/**/*.svg`, {
+        globstar: true,
+      });
+      for (const file of glob) {
+        const path = file.path.slice(baseDir.length + dir.length + 2);
+        paths.push(path);
+      }
+      Deno.writeTextFileSync(`src/data/${dir}.txt`, paths.join("\n"));
+    }
   }
-  Deno.writeTextFileSync(`src/data/${dir}.txt`, paths.join("\n"));
 }
